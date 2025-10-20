8c5d0424778e7ed64ac1993176b6af8963778d73
dev <romansyah26588@gmail.com>
2025-10-20 19:58:19 +0700

    Update route.ts

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 export const runtime = 'edge';
 export async function GET() {
-  return NextResponse.json({ message: "Good!" });
+  return NextResponse.json({ message: "Good Job!" });
 }
